026f40972d4d1b48a44efc57872b56bf7352e7cf
Michael Schulz <mi.schulz@hotmail.com>
2022-10-06 19:15:23 +0200

    Update page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
-import Navbar from '../components/Navbar';
+import Image from 'next/image';
 import Link from 'next/link';
+import Navbar from '../components/Navbar';
 
 function About() {
   return (
@@ -18,10 +19,24 @@ function About() {
               About
             </h1>
             <p className="description">
-              I studied nutritional science and genetics. Despite being a
-              science enthusiast I also love learning new languages (including
-              programming languages!). Do you want to work together?
+              My name is Michael and I am based in Vienna. I studied nutritional
+              science and genetics. Despite being a science enthusiast I also
+              love learning new languages (including programming languages!).
             </p>
+            <div>
+              <p className="tech-heading">Tech stack</p>
+              <div className="tech-wrapper">
+                <Image src="/html.svg" alt="" width="80" height="80" />
+                <Image src="/css.svg" alt="" width="80" height="80" />
+                <Image src="/tailwind.svg" alt="" width="80" height="80" />
+                <Image src="/sass.svg" alt="" width="80" height="80" />
+                <Image src="/js.svg" alt="" width="80" height="80" />
+                <Image src="/node.svg" alt="" width="80" height="80" />
+                <Image src="/react.svg" alt="" width="80" height="80" />
+                <Image src="/next.svg" alt="" width="80" height="80" />
+              </div>
+            </div>
+            <p>Do you want to work together?</p>
             <Link href="/contact">
               <a className="cta">Contact Me!</a>
             </Link>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 function Home() {
+  useEffect(() => {
+    document.title = 'Michael Schulz Portfolio';
+  }, []);
   return (
     <>
       <Head>
